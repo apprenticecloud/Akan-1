0c3f2d04d14acdf73aeabf4d9c3d269e55f22fe8
apprenticecloud <tech@sokofresh.co.ke>
2022-04-02 13:25:59 +0300

    Akan Name Evaluation Without display

--- akan.js
+++ akan.js
@@ -4,24 +4,37 @@ function processFormData(){
     const female = document.getElementById('femaleOption');
     var dayIndex = new Date(birthDate).getDay();
 
-    if(male.selected){
-        var gender = "Male"
-        
-    }else if(female.selected){
-        var gender = "Female"
-        const names = {Sunday:'Akosua',Monday:'Adwoa',Tuesday:'Abenaa',Wednesday:'Akua',Thursday:'Yaa',Friday:'Afua',Saturday:'Ama'}
-    }
-
+    //This one evaluates for the day of the week based on the day index
     const getDayName = (dayIndex) =>{
-        const akanName = {Sunday:'Kwasi',Monday:'Kwadwo',Tuesday:'Kwabena',Wednesday:'Kwaku',Thursday:'Yaw',Friday:'Kofi',Saturday:'Kwame'}
 	    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 	    return days[dayIndex];
-        
     }
     const dayName = getDayName(dayIndex)
 
+    //This code block evaluates for the Male Akan Name based on the dayindex above.
+    const getMaleAkanName = (dayIndex)=>{
+        const names = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+        return names[dayIndex];
+    }
     
-    console.log(dayName)
+    //This code block evaluates for the Feale Akan Name based on the dayindex above.
+    const getFemaleAkanName = (dayIndex)=>{
+        const names = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+        return names[dayIndex];
+    }
+
+    //This code block evaluates for the gender based on the gender selected
+    if(male.selected){
+        var gender = "Male"
+        var akanName = getMaleAkanName(dayIndex)
+
+    }else if(female.selected){
+        var gender = "Female"
+        var akanName = getFemaleAkanName(dayIndex)
+    }
+
+    //Display Akan Name
+    console.log(akanName)
 
     
 
